010edf73152b294f6b008299a7e40eef542237d8
Ákos <ytakos06@gmail.com>
2025-03-02 22:33:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ákos Channel</title>
+    
+        <!-- Favicon -->
+        <link rel="icon" href="favicon.png" type="image/x-icon">
+    
+        <link rel="stylesheet" href="styles.css">
+    </head>
+<body>
+
+    <!-- Dinamikusan betöltött header -->
+    <?php include 'header.html'; ?>
+
+    <main>
+        <section class="intro">
+            <h2>Üdvözöllek a YouTube csatornám weboldalán!</h2>
+            <p>Itt találhatók a legfrissebb információk, videók és minden egyéb, amit tudnod érdemes a csatornáról!</p>
+        </section>
+
+        <section class="cta">
+            <h2>Ne hagyd ki!</h2>
+            <p>Csatlakozz a közösséghez, és ne maradj le a legújabb tartalmakról!</p>
+            <a href="https://www.youtube.com/channel/UCQuBSCgwLjNTpCNxSpVInaA" class="btn-primary">Feliratkozás</a>
+        </section>
+    </main>
+
+    <!-- Dinamikusan betöltött footer -->
+    <?php include 'footer.html'; ?>
+
+</body>
+</html>
